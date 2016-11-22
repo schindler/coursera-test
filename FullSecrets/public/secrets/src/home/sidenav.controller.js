d199cbbe4486b52981ad4cdcd90d7073e145456a
schindler <fernando.schindler@gmail.com>
2016-11-22 02:28:58 -0300

    Bug fixes, users page

---
+++ FullSecrets/public/secrets/src/home/sidenav.controller.js
@@ -0,0 +1,71 @@
+/*!
+ * Side Navigator Controllers
+ * @license MIT
+ * v1.0
+ */
+(function(){
+  "use strict";
+   
+   angular
+      .module('AppSecrets')
+      .controller('sidenav-controller', SideNavController);	
+
+   SideNavController.$inject = ['$timeout', '$mdSidenav', '$log', 'session'];
+   
+   function SideNavController($timeout, $mdSidenav, $log, session) {
+        var ctrl = this;
+        ctrl.fullname = (session.getUser() || { name : ""}).name;
+		ctrl.toggle   = buildDelayedToggler('left'); 
+		ctrl.isOpen   = function(navID){
+		  return $mdSidenav(navID).isOpen();
+		};
+   	 
+
+		/**
+		 * Supplies a function that will continue to operate until the
+		 * time is up.
+		 */
+		function debounce(func, wait, context) {
+		  var timer;
+
+		  return function debounced() {
+		    var context = ctrl,
+		        args = Array.prototype.slice.call(arguments);
+		    $timeout.cancel(timer);
+		    timer = $timeout(function() {
+		      timer = undefined;
+		      func.apply(context, args);
+		    }, wait || 10);
+		  };
+		}
+
+		/**
+		 * Build handler to open/close a SideNav; when animation finishes
+		 * report completion in console
+		 */
+		function buildDelayedToggler(navID) {
+		  return debounce(function() {
+		    // Component lookup should always be available since we are not using `ng-if`
+            ctrl.fullname = (session.getUser() || { name : ""}).name;
+		    $mdSidenav(navID)
+		      .toggle()
+		      .then(function () {
+				
+		        $log.debug("toggle " + navID + " is done");
+		      });
+		  }, 200);
+		}
+
+		function buildToggler(navID) {
+		  return function() {
+		    // Component lookup should always be available since we are not using `ng-if`
+		    $mdSidenav(navID)
+		      .toggle()
+		      .then(function () {
+		        $log.debug("toggle " + navID + " is done");
+		      });
+		  }
+		}
+   }
+})(); 
+  
---
+++ FullSecrets/public/secrets/src/loading/loading.component.js
@@ -0,0 +1,33 @@
+(function() {
+"use strict";
+
+angular.module('AppSecrets')
+.component('appLoading', {
+  template: '<md-progress-linear ng-disabled="!$ctrl.show" md-mode="indeterminate"></md-progress-linear>',
+  controller: LoadingController
+});
+
+
+LoadingController.$inject = ['$rootScope'];
+function LoadingController ($rootScope) {
+  var $ctrl = this;
+  var listener;
+  
+   console.log("LoadingController: on" );
+
+  $ctrl.$onInit = function() {
+    $ctrl.show = false;
+    listener = $rootScope.$on('spinner:activate', onSpinnerActivate);
+  };
+
+  $ctrl.$onDestroy = function() {
+    listener();
+  };
+
+  function onSpinnerActivate(event, data) {
+    console.log("onSpinnerActivate: " + data.on);
+    $ctrl.show = data.on;
+  }
+}
+
+})();
